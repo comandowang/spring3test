8545ca1d17e77e6cad8b656b2a2780af24ba8e4c
comando <comando@126.com>
2017-10-17 14:38:37 +0800

    test

---
+++ src/main/java/TestBean.java
@@ -0,0 +1,22 @@
+public class TestBean {
+
+    private String beanName = "testbean";
+
+    private String country;
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/XmlBeanFactoryTests.java
@@ -0,0 +1,17 @@
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Created by comando on 2017/10/13.
+ */
+public class XmlBeanFactoryTests {
+    public static void main(String[] args) {
+        ClassPathResource resource = new ClassPathResource("XmlBeanFactoryTests-localCollectionsUsingXsd.xml");
+
+        XmlBeanFactory factory = new XmlBeanFactory(resource);
+
+        TestBean testBean = (TestBean)factory.getBean("testBean");
+
+        System.out.println(testBean.getBeanName());
+    }
+}
